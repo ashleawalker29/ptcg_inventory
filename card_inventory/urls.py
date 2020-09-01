8b92143d446714ec18eceece8ab114747d2b31bd
Ashlea Walker <ashleawalker29@gmail.com>
2020-09-01 15:16:38 -0600

    Created `card_inventory/set_name/` view.
    - Created the base HTML template for the `card_inventory` app.
    - Created the card details HTML template for the `card_inventory/set_name/` webpage.
    - Created the view method to render the `card_inventory/set_name/` webpage.
    - Added the `card_inventory/set_name/` url pattern to `urls.py`.

--- card_inventory/urls.py
+++ card_inventory/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('<str:set_name>/', views.card_details_by_set, name='card_details')
 ]
--- card_inventory/views.py
+++ card_inventory/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from card_inventory.models import Cards
+
+def card_details_by_set(request, set_name):
+    set_name_clean = set_name.replace('_', ' ').title()
+    cards = Cards.objects.filter(set_name=set_name_clean)
+
+    return render(request, 'card_details.html',
+        {'card_objs': cards,
+         'set_name': set_name_clean})
