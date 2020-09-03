7fe1e82f3c479a684a5dea25384a7d41e6f46f75
Ashlea Walker <ashleawalker29@gmail.com>
2020-09-03 15:08:15 -0600

    Retrieve card quantities (normal, reverse, and holo) per set.

--- card_inventory/views.py
+++ card_inventory/views.py
@@ -9,6 +9,18 @@ def sets_index(request):
              'logo_url': Set.logo_url, 'symbol_url': Set.symbol_url}
             for Set in pokemontcgsdk.Set.all()]
 
+    for s in sets:
+        try:
+            quantity_normal = sum([card.quantity_normal for card in Cards.objects.filter(set_name=s['name'])])
+            quantity_reverse = sum([card.quantity_reverse for card in Cards.objects.filter(set_name=s['name'])])
+            quantity_holo = sum([card.quantity_holo for card in Cards.objects.filter(set_name=s['name'])])
+        except:
+            quantity_normal, quantity_reverse, quantity_holo = None, None, None
+
+        s['quantity_normal'] = quantity_normal
+        s['quantity_reverse'] = quantity_reverse
+        s['quantity_holo'] = quantity_holo
+
     return render(request, 'sets_index.html', {'sets': sets})
 
 def cards_by_set(request, set_name):
