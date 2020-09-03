ecafab7a682e825240b1499a0564d0b0ce1ce87c
Ashlea Walker <ashleawalker29@gmail.com>
2020-09-02 20:51:28 -0600

    Changed the main page to default to the card_inventory app.

--- ptcg_inventory/urls.py
+++ ptcg_inventory/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('card_inventory/', include('card_inventory.urls'))
+    path('', include('card_inventory.urls'))
 ]
