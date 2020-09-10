18f2785db101348eff23f10510a4b3117ec3f5eb
ashleawalker29 <ashleawalker29@gmail.com>
2020-09-10 16:07:55 -0600

    Added default psql server for GitHub WorkFlows

--- ptcg_inventory/settings.py
+++ ptcg_inventory/settings.py
@@ -101,6 +101,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+           'ENGINE': 'django.db.backends.postgresql',
+           'NAME': 'github_actions',
+           'USER': 'postgres',
+           'PASSWORD': 'postgres',
+           'HOST': '127.0.0.1',
+           'PORT': '5432',
+        }
+    }
+
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
