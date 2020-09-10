527896b5fa42df94429c104df75fc0a95d1c018c
Ashlea Walker <ashleawalker29@gmail.com>
2020-09-10 15:45:38 -0600

    Created initial test for homepage.

--- card_inventory/tests.py
+++ card_inventory/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class CardInventoryTests(TestCase):
+
+    def test_cards_by_set_view(self):
+        response = self.client.get('')
+
+        self.assertEqual(response.status_code, 200)
