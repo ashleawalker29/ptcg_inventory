7a82e8e6e2ae2335a8fe01e43ec698f159793f5e
Ashlea Walker <ashleawalker29@gmail.com>
2020-09-11 15:00:58 -0600

    Added App test.

--- card_inventory/tests.py
+++ card_inventory/tests.py
@@ -1,6 +1,13 @@
+from django.apps import apps
 from django.test import TestCase
 
+from card_inventory.apps import CardInventoryConfig
+
 class CardInventoryTests(TestCase):
+
+    def test_card_inventory_app(self):
+        self.assertEqual(CardInventoryConfig.name, 'card_inventory')
+        self.assertEqual(apps.get_app_config('card_inventory').name, 'card_inventory')
 
     def test_cards_by_set_view(self):
         response = self.client.get('')
