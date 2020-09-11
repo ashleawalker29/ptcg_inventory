f705cc94bd1755c6fdec7c7ce55b8307330e6d90
Ashlea Walker <ashleawalker29@gmail.com>
2020-09-11 15:43:08 -0600

    Added Cards-By-Set-Views Test
    Renamed test_cards_by_sets_view to test_sets_index since that name
    was misleading.
    Added test_cards_by_set_views test to test all cards_by_set subpages
    generated by the pokemontcgsdk api.

--- card_inventory/tests.py
+++ card_inventory/tests.py
@@ -1,5 +1,6 @@
 from django.apps import apps
 from django.test import TestCase
+import pokemontcgsdk
 
 from card_inventory.apps import CardInventoryConfig
 
@@ -9,7 +10,16 @@ class CardInventoryTests(TestCase):
         self.assertEqual(CardInventoryConfig.name, 'card_inventory')
         self.assertEqual(apps.get_app_config('card_inventory').name, 'card_inventory')
 
-    def test_cards_by_set_view(self):
+    def test_sets_index(self):
         response = self.client.get('')
 
         self.assertEqual(response.status_code, 200)
+
+    def test_cards_by_set_views(self):
+        set_names = [Set.name for Set in pokemontcgsdk.Set.all()]
+
+        for set_name in set_names:
+            set_name_link = set_name.replace(' ', '_')
+            response = self.client.get('/%s/' % set_name_link)
+
+            self.assertEqual(response.status_code, 200)
