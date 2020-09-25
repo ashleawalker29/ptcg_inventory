7ce5fef59d8a5ca6eb577617c17da038b2ea65c8
Ashlea Walker <ashleawalker29@gmail.com>
2020-09-25 17:48:14 -0600

    Created Duplicate Cards view.
    Changed url redirects to be more verbose.
    Added duplicate card view to list all cards owned that have more
    than 4 instances.
    Created duplicate_cards.html to go along with the view.

--- card_inventory/urls.py
+++ card_inventory/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.sets_index, name='sets_index'),
-    path('<str:set_name>/', views.cards_by_set, name='card_details')
+    path('index/', views.sets_index, name='sets_index'),
+    path('index/<str:set_name>/', views.cards_by_set, name='card_details'),
+    path('duplicate_cards/', views.duplicate_cards, name='duplicate_cards')
 ]
