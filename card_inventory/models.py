4ad573d793652c77c8f217ca6ee3c78c542ef30a
Ashlea Walker <ashleawalker29@gmail.com>
2020-09-28 23:02:39 -0600

    Changed DB schema and the views that interact with it.
    Within the DB schema:
    - Removed unused ENUMs.
    - Added `Sets` table to contain all information about current sets.
    - Removed unecessary `card_type` column from `Cards` table as that
      information can be obtained through the API.
    - Renamed `set_name` column to `set_code` column in the `Cards` table.
    - Changed `set_code` column from the `Cards` table to be a foreign
      key connection to the `Sets` table.
    
    Within `views.py`:
    - Added references to the `Sets` table where appropriate instead of
      the out-dated `set_name` from the `Cards` table.
    - Changed the information being rendered from `cards_by_set`. This
      now includes more information about the set that can be viewed
      on the webpage.
    
    Within `cards_by_set.html`:
    - Changed all Set-based references to using the new `set_info`
      variable.
    - Dynamically display the card's number by concatenating the card
      number and the set's max cards (not including secret rare's).
      An example of this is card number 8 from the Base set. The Base
      set has 102 max cards. Therefore the concatenated string of this
      would be '8/102' within the `Card Number` column on the webpage.

--- card_inventory/models.py
+++ card_inventory/models.py
@@ -2,19 +2,17 @@ from enum import Enum
 
 from django.db import models
 
-class SetNames(Enum):
-    BASE_SET = 'Base Set'
 
-class CardTypes(Enum):
-    GRASS = 'Grass'
-    FIRE = 'Fire'
-    WATER = 'Water'
+class Sets(models.Model):
+    set_name = models.CharField(null=False, unique=True, max_length=255)
+    set_code = models.CharField(primary_key=True, max_length=10)
+    max_cards = models.IntegerField(null=False)
+
 
 class Cards(models.Model):
-    set_name = models.CharField(max_length=5, choices=[(tag, tag.value) for tag in SetNames])
+    set_code = models.ForeignKey(Sets, on_delete=models.CASCADE)
     card_number = models.TextField(null=False)
     card_name = models.TextField(null=False)
-    card_type = models.CharField(max_length=5, choices=[(tag, tag.value) for tag in CardTypes])
     quantity_normal = models.IntegerField(null=True, default=0)
     quantity_reverse = models.IntegerField(null=True, default=0)
     quantity_holo = models.IntegerField(null=True, default=0)
--- card_inventory/views.py
+++ card_inventory/views.py
@@ -1,31 +1,33 @@
 from django.shortcuts import render
 import pokemontcgsdk
 
-from card_inventory.models import Cards
+from card_inventory.models import Cards, Sets
 
 def sets_index(request):
-    sets = [{'name': Set.name, 'link': Set.name.replace(' ', '_'),
+    sets = [{'name': Set.name, 'code': Set.code , 'link': Set.name.replace(' ', '_'),
              'series': Set.series, 'release_date': Set.release_date,
              'logo_url': Set.logo_url, 'symbol_url': Set.symbol_url}
             for Set in pokemontcgsdk.Set.all()]
 
     for s in sets:
         s['quantity_normal'] = sum(
-            [card.quantity_normal or 0 for card in Cards.objects.filter(set_name=s['name'])])
+            [card.quantity_normal or 0 for card in Cards.objects.filter(set_code=s['code'])])
         s['quantity_reverse'] = sum(
-            [card.quantity_reverse or 0 for card in Cards.objects.filter(set_name=s['name'])])
+            [card.quantity_reverse or 0 for card in Cards.objects.filter(set_code=s['code'])])
         s['quantity_holo'] = sum(
-            [card.quantity_holo or 0 for card in Cards.objects.filter(set_name=s['name'])])
+            [card.quantity_holo or 0 for card in Cards.objects.filter(set_code=s['code'])])
 
     return render(request, 'sets_index.html', {'sets': sets})
 
 def cards_by_set(request, set_name):
     set_name_clean = set_name.replace('_', ' ')
-    cards = Cards.objects.filter(set_name=set_name_clean)
+    set_info = Sets.objects.get(set_name=set_name_clean)
+
+    cards = Cards.objects.filter(set_code=set_info.set_code)
 
     return render(request, 'cards_by_set.html',
-        {'cards': cards,
-         'set_name': set_name_clean})
+        {'set_info': set_info,
+         'cards': cards})
 
 def duplicate_cards(request):
     cards = Cards.objects.all()
